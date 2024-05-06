6393d796af6978a8b0e740c849be215b5ece3863
Shoaibsolanki <118595013+Shoaibsolanki73@users.noreply.github.com>
2024-05-06 19:50:46 +0530

    new data base

--- db.js
+++ db.js
@@ -17,7 +17,7 @@ const mongoose = require('mongoose');
 const connectToMongo = async () => {
     try{ 
         mongoose.set("strictQuery", false);
-        await mongoose.connect(`mongodb://127.0.0.1:27017/KumeshProject`);
+        await mongoose.connect(`mongodb+srv://shoaibsolanki73:shoaib123@cluster0.qixovaf.mongodb.net/`);
         console.log("connected to mongo succesfully");
     } catch (err){
         console.log('Failled connected to mongodb', err);
